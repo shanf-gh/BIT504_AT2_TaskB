db25c39d999b817f8c4093ca1e5b928665987113
shanf-gh <shan.fabien@gmail.com>
2021-12-31 16:52:07 +1300

    Changed BALL_COLOUR from BLACK to WHITE

--- src/Ball.java
+++ src/Ball.java
@@ -1,7 +1,7 @@
 import java.awt.Color;
 
 public class Ball extends Sprite {
-	private static final Color BALL_COLOUR = Color.BLACK;
+	private static final Color BALL_COLOUR = Color.WHITE;
 	private static final int BALL_WIDTH = 25;
 	private static final int BALL_HEIGHT = 25;
 	
